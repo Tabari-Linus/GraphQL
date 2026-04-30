804c169b924e8358fb64dc7b131d5c4f14463e85
mr_lii <leetamparega@gmail.com>
2026-04-30 14:13:19 +0000

    refactor: change totalAmount type from Double to BigDecimal in CreateExpenseInput

--- demo/src/main/java/com/example/demo/dtos/CreateExpenseInput.java
+++ demo/src/main/java/com/example/demo/dtos/CreateExpenseInput.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record CreateExpenseInput(
@@ -12,7 +13,7 @@ public record CreateExpenseInput(
         String description,
         @NotNull(message = "Total amount is required expense")
         @Positive(message = "Total amount  for expense must be a positive value")
-        Double totalAmount,
+        BigDecimal totalAmount,
         List<ItemInput> itemInputs
 ) {
 }
